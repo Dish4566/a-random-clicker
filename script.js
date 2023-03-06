622b2ee4d2d3272e7da6930272ad6a0bddda5453
Dish4566 <rsondhi3132@gmail.com>
2023-03-06 17:24:14 -0600

    e

--- script.js
+++
@@ -1,9 +0,0 @@
-let counter = 0;
-const button = document.getElementsByClassName('block')[0];
-
-
-/* Main funct */
-function clicked() {
-  counter += 1;
-  document.getElementById('score').innerText = counter;
-}
